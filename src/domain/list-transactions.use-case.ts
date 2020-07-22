b62259dc0bbe90960761399495b93683b8e08e58
RiczCalixto <rcalixtos@gmail.com>
2020-07-22 17:24:12 -0300

    List transactions

--- src/domain/list-transactions.use-case.ts
+++ src/domain/list-transactions.use-case.ts
@@ -1,7 +1,6 @@
 import api from '../services/api';
 import { mapToDashboardInformation } from './list-transactions.mapper';
 import { DashboardInformation } from '../model/dashboard.model';
-import { useEffect } from 'react';
 
 export const listTransactionUseCase = async (): Promise<
   DashboardInformation
